570916ebaf2e93939e6650883f9fbc9386b65ce7
mbuchner <markus.buchner@gmail.com>
2019-09-09 22:34:41 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { GridService } from './services/grid.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ GridService ],
 })
 export class AppModule { }
--- src/app/services/grid.service.ts
+++ src/app/services/grid.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
-export class IdleService {
+export class GridService {
 
   constructor(
   ) { }
