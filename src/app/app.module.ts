a71020e30101e2386e2816048945438d5973fe42
mbuchner <markus.buchner@gmail.com>
2019-09-09 23:25:47 +0200

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { GridService } from './services/grid.service';
 import { RobotService } from './services/robot.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ GridService, RobotService ],
 })
--- src/app/services/grid.service.ts
+++ src/app/services/grid.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({ providedIn: 'root' })
 export class GridService {
 
-  constructor(
-  ) { }
+  constructor(private http: HttpClient) {
+        this.getJSON().subscribe(data => {
+            console.log(data);
+        });
+    }
+
+    public getJSON(): Observable<any> {
+        return this.http.get("./assets/mydata.json");
+    }
 
 
 }
