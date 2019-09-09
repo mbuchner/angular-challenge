b338570dade4615422565ea70b9a551f51191e9b
mbuchner <markus.buchner@gmail.com>
2019-09-09 22:39:09 +0200

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GridService } from './services/grid.service';
+import { RobotService } from './services/robot.service';
 
 @Component({
   selector: 'my-app',
@@ -8,6 +9,6 @@ import { GridService } from './services/grid.service';
 })
 export class AppComponent  {
   name = 'Angular';
-  constructor(private gridService: GridService){}
+  constructor(private gridService: GridService, private robotService: RobotService){}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GridService } from './services/grid.service';
+import { RobotService } from './services/robot.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [ GridService ],
+  providers: [ GridService, RobotService ],
 })
 export class AppModule { }
