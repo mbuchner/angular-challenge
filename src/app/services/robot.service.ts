2f6ca3881bba1375e1249834a29354065b7476bf
mbuchner <markus.buchner@gmail.com>
2019-09-10 16:50:32 +0200

    3 files modified

--- src/app/services/robot.service.ts
+++ src/app/services/robot.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class RobotService {
 
-  constructor() { }
+  position: Location;
 
+  location: {
+    x: number;
+    y: number;
+  }
+
+  constructor() {
+    this.location.x = 1;
+    this.location.y = 2;
+  }
+
+  moveUp() {
+    if (this.location.y != 0) {
+      this.location.y = this.location.y - 1;
+    } else {
+      console.log("out of bounds");
+    }
+  }
+
+  moveDown() {
+    if (this.location.y != 15) {
+      this.location.y = this.location.y + 1;
+    } else {
+      console.log("out of bounds");
+    }
+  }
+
+  moveLeft() {
+    if (this.location.x != 0) {
+      this.location.x = this.location.x - 1;
+    } else {
+      console.log("out of bounds");
+    }
+  }
+
+  moveRight() {
+    if (this.location.x != 15) {
+      this.location.x = this.location.x + 1;
+    } else {
+      console.log("out of bounds");
+    }
+  }
 }
