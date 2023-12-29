0e8c76d5cc797920b50af9680596aff6f2137f5b
mbuchner <markus.buchner@gmail.com>
2023-12-29 17:30:02 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,25 @@ import { Location } from './model/location.model';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   robotCell: string;
 
-  gridData$ = this.gridService.gridData$;
+  gridData$: any;
 
   instructionSet: string[];
 
-  instructionSet$ = this.gridService.instructionSet$;
+  instructionSet$: any;
 
   measures: string;
 
   play() {
     // So far so good!
-    // Welcome to the mars robot challenge! We already have prepared  
-    // the services and data for you but this function is still missing. 
+    // Welcome to the mars robot challenge! We already have prepared
+    // the services and data for you but this function is still missing.
     // It can be triggered by pressing the play button in the HTMLv view.
-    // Based on the "instructionSet" ("RIGHT", "UP", "RIGHT", ...) 
+    // Based on the "instructionSet" ("RIGHT", "UP", "RIGHT", ...)
     // you should navigate the robot on mars and measure after every move.
     // The "robotService" provides you with the required functions.
     // (moveUp, moveDown, moveLeft, moveRight, measure)
@@ -37,14 +36,20 @@ export class AppComponent implements OnInit {
     //...
   }
 
-  constructor(private gridService: GridService, private robotService: RobotService) { }
+  constructor(
+    private gridService: GridService,
+    private robotService: RobotService
+  ) {
+    this.gridData$ = this.gridService.gridData$;
+    this.instructionSet$ = this.gridService.instructionSet$;
+  }
 
   ngOnInit() {
     this.robotService.location$.subscribe((location: Location) => {
-      this.robotCell = location.x + "x" + location.y;
+      this.robotCell = location.x + 'x' + location.y;
     });
     this.robotService.measures$.subscribe((measures: any[]) => {
-      this.measures = measures && measures.join(' ') || '\u00a0';
+      this.measures = (measures && measures.join(' ')) || '\u00a0';
     });
     this.gridService.instructionSet$.subscribe((instructions: any[]) => {
       this.instructionSet = instructions;
